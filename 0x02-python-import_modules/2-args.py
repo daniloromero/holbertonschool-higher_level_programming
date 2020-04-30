6f9a9efa0c3cb03212ded36581f4c4de4a0d2974
daniloromero <1601@holbertonschool.com>
2020-04-30 20:51:52 +0000

    added semicolon at 3 condition print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif argc == 1:
         print("{} argument:".format(argc))
     else:
-        print("{} arguments".format(argc))
+        print("{} arguments:".format(argc))
     for i in range(argc):
         print("{:d}: {:}".format(i + 1, argv[i + 1]))
