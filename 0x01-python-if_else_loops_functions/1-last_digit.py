89f39a6189e7d230a2ac18f9df0e4e1016e906ce
daniloromero <1601@holbertonschool.com>
2020-05-01 03:01:05 +0000

    moved condition for 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,11 @@ if number >= 0:
 else:
     last = (number * -1 % 10) * -1
 
-print("Last digit of {} is {}".format(number, last), end="")
+print("Last digit of {} is {} ".format(number, last), end="")
 
-if last == 0:
-    print("and is 0")
-elif last > 5:
+if last > 5:
     print("and is greater than 5")
+elif last == 0:
+    print("and is 0")
 else:
     print("and is less than 6 and not 0")
