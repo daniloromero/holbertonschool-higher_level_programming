9d7ff68d5964da52fc50732b666c57d42defe834
daniloromero <1601@holbertonschool.com>
2020-05-02 03:11:38 +0000

    Added missing whitespace on line 5

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 from sys import argv
 if __name__ == "__main__":
     add = 0
-    for i in range(1,len(argv)):
+    for i in range(1, len(argv)):
         add += int(argv[i])
     print("{:d}".format(add))
