055596da49a59b52451c2052b7e2b98a61ce342b
daniloromero <1601@holbertonschool.com>
2020-05-07 03:24:21 +0000

    Files creation

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * is_palindrome - Checks palidrom list.
+ * @head: Single Linked list
+ * Return: 0 if it's not a palindrome, 1 if it's a palindrome.
+ */
+int is_palindrome(listint_t **head)
+{
+}
