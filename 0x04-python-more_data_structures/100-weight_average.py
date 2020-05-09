f89c63a8c8ab58243cce87a9162f5d8049a49c96
daniloromero <1601@holbertonschool.com>
2020-05-09 00:41:25 +0000

    tested on console

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return (0)
+    total = sum(i[0] * i[1] for i in my_list)
+    div = sum(i[1] for i in my_list)
+    return(total / div)
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    rom_dec = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    for k in roman_string:
+        if k not in rom_dec:
+            return 0
+    num = 0
+    prev = rom_dec.get(roman_string[0])
+    for k in roman_string:
+        value = rom_dec.get(k)
+        if prev >= value:
+            num += value
+        elif prev <= value:
+            num = num + value - prev * 2
+        prev = value
+    return num
