207c4bebaf522c250386ae1bc633273428d57867
daniloromero <1601@holbertonschool.com>
2020-05-18 17:31:10 +0000

    Removed pass expresion

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
     except IndexError:
-        pass
+        i -= 1
     finally:
         print()
-    return (i)
+    return (i + 1)
