78da91cab0562b1e619451070426ef8565399e48
daniloromero <1601@holbertonschool.com>
2020-05-18 17:48:08 +0000

    Added type cast to value

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        answer = True
-    except:
-        answer = False
-    return answer
+        integer = int(value)
+        print("{:d}".format(integer))
+        return True
+    except ValueError:
+        return False
