02619d9ea616b822fd935af425dd4d880a29dbc1
daniloromero <1601@holbertonschool.com>
2020-05-18 17:32:46 +0000

    Removed IndexError expression

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-    except IndexError:
+    except:
         i -= 1
     finally:
         print()
