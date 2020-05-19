89b9b761f9bedac3beb88e8c85c6e61fc5732fb4
daniloromero <1601@holbertonschool.com>
2020-05-19 01:13:15 +0000

    added whitespace around operator

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            counter +=1
+            counter += 1
         except (TypeError, ValueError):
             pass
     print("")
