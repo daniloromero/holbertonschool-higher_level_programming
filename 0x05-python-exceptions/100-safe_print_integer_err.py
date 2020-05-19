200fa679d16e8ed25a57d2aa2a69b500c485b35d
daniloromero <1601@holbertonschool.com>
2020-05-19 03:53:57 +0000

    1st version teste on console

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as othertype:
+        sys.stderr.write("Exeption: {}\n".format(othertype))
+        return False
