56b5e30fecbe9e3a0e8c2b077b15af2fda00620f
daniloromero <1601@holbertonschool.com>
2020-05-19 04:31:03 +0000

    1st version

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if(i > a):
+            result += a ** b / i
+        except Exception('Too far')
+            result = b + a
+        break
+    return result
