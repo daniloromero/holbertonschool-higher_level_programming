c2b1513305090a6050dbb2b5400aa3df3c76cef8
daniloromero <1601@holbertonschool.com>
2020-05-20 04:30:05 +0000

    1st version

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" class Square """
+
+
+class Square:
+    """Private instance attribute: size"""
+    def __init__(self, size=0, position=(0,0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    @position
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (value == tuple and len(value) == 2):
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
