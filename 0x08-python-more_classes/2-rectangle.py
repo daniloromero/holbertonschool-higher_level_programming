98d8ae5e123b6798cfce299ea0d6ab2dc6d78cf5
daniloromero <1601@holbertonschool.com>
2020-05-25 23:36:07 +0000

    1st version tested on console

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Class constructor for a Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Private attribute getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Private attribute setter """
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @property
+    def height(self, value):
+        """ Private attribute getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Private attribute setter """
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    def area(self):
+        """ Public method for area of Rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Public method for perimenter of Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return ((self.__width * 2) + (self.__height * 2))
---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Class constructor for a Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Private attribute getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Private attribute setter """
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ Private attribute getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Private attribute setter """
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    def area(self):
+        """ Public method for area of Rectangle """
+        return self.width * self.height
+
+    def perimeter(self):
+        """ Public method for perimenter of Rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return ((self.width * 2) + (self.height * 2))
+
+    def __str__(self):
+        """ str method for a Rectangle """
+        rectgl = ""
+        if self.__width == 0:
+            return rectgl
+        else:
+            for i in range(self.__height):
+                rectgl += "{}".format('#' * self.__width)
+                if i != (self.__width - 1):
+                    rectgl += "\n"
+        return rectgl
