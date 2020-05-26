973667e1b73fb6c72f2e842a34f562311eefd1c0
daniloromero <1601@holbertonschool.com>
2020-05-26 00:12:12 +0000

    rearrange precedence, heightbefore width

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,21 +9,6 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self):
-        """ Private attribute getter """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ Private attribute setter """
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        elif value < 0:
-            raise ValueError('width must be >= 0')
-        else:
-            self.__width = value
-
-    @property
     def height(self, value):
         """ Private attribute getter """
         return self.__height
@@ -37,3 +22,18 @@ class Rectangle:
             raise ValueError('height must be >= 0')
         else:
             self.__height = value
+
+    @property
+    def width(self):
+        """ Private attribute getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Private attribute setter """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
