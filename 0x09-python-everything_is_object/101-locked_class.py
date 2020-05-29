38ac0c985434ad437b9e7e0995f5a49d27e5451b
daniloromero <1601@holbertonschool.com>
2020-05-29 04:00:24 +0000

    added header docstring

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Create LockedClass"""
+
+
 class LockedClass:
     """ Class locked to first_name, no __dict__ """
-    __slots__ = 'first_name'
+    __slots__ = ['first_name']
