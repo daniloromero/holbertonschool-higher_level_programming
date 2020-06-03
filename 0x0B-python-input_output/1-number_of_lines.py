dbfb515bbb65b664bfabedbb2b6288251bcadb8e
daniloromero <1601@holbertonschool.com>
2020-06-03 23:16:52 +0000

    Added header docstring

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Get number of lines of text file """
 
 
 def number_of_lines(filename=""):
-    """Mehtod returns # lines of a text file"""
+    """Method returns # lines of a text file"""
     with open(filename) as Myfile:
         num_lines = len(Myfile.readlines())
         return num_lines
