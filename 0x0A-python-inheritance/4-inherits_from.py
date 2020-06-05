fd09790894f9ef529407b63b7e2ec395adf066b7
daniloromero <1601@holbertonschool.com>
2020-06-05 02:11:56 +0000

    pycodestyled

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,8 @@ def inherits_from(obj, a_class):
     that inherited (directly or indirectly) from the specified class ;
     otherwise False
     """
-    if type(obj, == a_class):
+    if type(obj) == a_class:
         return False
     else:
-        isinstance(obj, a_class):
+        isinstance(obj, a_class)
+        return True
