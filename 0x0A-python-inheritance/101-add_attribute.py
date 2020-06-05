3de3f55b05f17c5fd019ed5e88a41e3134f86ad4
daniloromero <1601@holbertonschool.com>
2020-06-05 02:39:51 +0000

    Added docstring

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""adds a new attribute to an object if it’s possible"""
 
 
 def add_attribute(self, attr, value):
     """method adds a new attribute to an object if it’s possible"""
     if not hasattr(self, "__dict__"):
         raise TypeError("can't add new attribute")
-    setattr(self, attr, value)
+    else:
+        setattr(self, attr, value)
