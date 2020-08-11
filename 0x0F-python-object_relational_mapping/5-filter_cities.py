d37f702f8f0a5ebe4eda9bf74704bda074882ae4
daniloromero <1601@holbertonschool.com>
2020-08-11 13:30:44 +0000

    Case sensitive match => BYNARY

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     SELECT cities.name
     FROM cities
     JOIN states ON cities.state_id = states.id
-    WHERE states.name=%s
+    WHERE BYNARY states.name=%s
     ORDER BY cities.id ASC;
     """
     c.execute(query, (stateq, ))
