97772b75bab79d9a73a3c173cb938eeafd0f6d6c
daniloromero <1601@holbertonschool.com>
2020-08-24 23:23:55 +0000

    First commit

---
+++ 0x11-python-network_1/0-htbn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        answer = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(answer)))
+        print('\t- content: {}'.format(answer))
+        print('\t- utf8 content: {}'.format(answer.decode()))
