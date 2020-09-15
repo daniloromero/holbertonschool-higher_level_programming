3bca6e7a725d0327c002e9f390720730e2042380
daniloromero <1601@holbertonschool.com>
2020-09-15 22:54:26 +0000

    Add task

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myVar =  process.argv.slice(2);
+const x = Number(myVar[0])
+if (isNaN(x)) {
+	console.log('Missing number of occurrences');
+} else {
+	for (let i = 0; i < x; i++) {
+	console.log('C is fun');
+	}
+}
