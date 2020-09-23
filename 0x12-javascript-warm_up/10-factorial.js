8ea516c41715626a437f61ce93ed98be8c85e0fc
daniloromero <danilo@LAPTOP-GDLDIUAI.localdomain>
2020-09-23 00:26:42 -0500

    Semistandard style

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
-const myVar =  process.argv.slice(2);
+const myVar = process.argv.slice(2);
 const x = parseInt(myVar[0]);
-function factorial(y) {
-	if (isNaN(y)) {
-		return 1;
-	}
-	if (y === 0) {
-		return 1;
-	}
-	else {
-		return (y *factorial(y - 1));
-	}
+function factorial (y) {
+  if (isNaN(y)) {
+    return 1;
+  }
+  if (y === 0) {
+    return 1;
+  } else {
+    return (y * factorial(y - 1));
+  }
 }
 console.log(factorial(x));
