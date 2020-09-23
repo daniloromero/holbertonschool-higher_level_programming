0d5d4e12296fdd3c69931b449f7d7a1a4a643eb1
daniloromero <danilo@LAPTOP-GDLDIUAI.localdomain>
2020-09-23 00:29:16 -0500

    Semistandard style

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascrit i amazing'];
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (const i of myArray) {
   console.log(i);
 }
