7c2217ab9798cf30d21595d119b4eeb3b067983f
daniloromero <danilo@LAPTOP-GDLDIUAI.localdomain>
2020-10-02 21:19:44 -0500

    Typo fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const myVar = process.argv.slice(2);
 if (myVar.length === 0) {
   console.log('No argument');
-} else if (myVar.lenght === 1) {
+} else if (myVar.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
