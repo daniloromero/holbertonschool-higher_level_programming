2e409bafd8a607c5be0ef29169ca38a1cb4933ad
daniloromero <danilo@LAPTOP-GDLDIUAI.localdomain>
2020-10-02 23:40:24 -0500

    Second max in array

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myVar = process.argv.slice(2);
+if (myVar.length <= 1) {
+  console.log(0);
+} else {
+  const myArr = myVar.map(x => parseInt(x));
+  myArr.sort((a, b) => a - b);
+  console.log(myArr);
+  myArr.pop();
+  console.log(Math.max(...myArr));
+}
