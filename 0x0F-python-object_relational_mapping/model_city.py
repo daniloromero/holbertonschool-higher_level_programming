2b89656b65696b8afdd57708a76e1863ab255f8d
daniloromero <1601@holbertonschool.com>
2020-12-13 23:02:30 -0500

    Add model_city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Model for State class"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeingKey, MetaData
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Class city"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeingKey('state.id'), nullable=False)
