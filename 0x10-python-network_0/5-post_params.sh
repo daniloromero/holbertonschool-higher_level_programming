13204e1aff2a717e02ffa2b854ef16fb8330db61
daniloromero <1601@holbertonschool.com>
2020-12-18 19:04:59 -0500

    Add task + shellcheck

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# script takes a URL, sends a GET request to URL, displays body of response
+curl -sL -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD""$1"
