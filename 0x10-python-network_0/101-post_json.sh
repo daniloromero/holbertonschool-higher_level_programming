35250a8570c35125ebfef704a3d8d751fbd7656b
daniloromero <1601@holbertonschool.com>
2020-12-18 21:32:19 -0500

    Chenged flag -T to -d @

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script sends DELETE request 2 URL passed as argument,displays body response
-curl -s -X POST -H "Content-Type: application/json" -T "$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
