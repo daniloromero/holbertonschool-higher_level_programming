c6bc351f92b4be408abdffb22363816c783b09fa
daniloromero <1601@holbertonschool.com>
2020-12-18 19:42:21 -0500

    Add task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Test function find_peak """
+def find_peak(list_of_integers):
+    """find peak in list_of_integers"""
+    if len(list_of_integers) == 0:
+        return None
+    else:
+        list_of_integers.sort()
+        return list_of_integers[-1] 
+
