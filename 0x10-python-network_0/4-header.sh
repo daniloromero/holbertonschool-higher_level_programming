ce2771c5f0051601c860059e65b341411551aad3
daniloromero <1601@holbertonschool.com>
2020-12-18 19:10:46 -0500

    removed -I flag

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes a URL, sends a GET request to URL, displays body of response
-curl -sI -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
