5a150fcb6d3716ccd880f6edf08e695e604a476e
daniloromero <1601@holbertonschool.com>
2020-12-18 21:16:49 -0500

    Just 3 line for the script

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script sends DELETE request 2 URL passed as argument,displays body response
-curl -s -H "Content-Type: application/json" "$1"\
-	  --request POST -T "$2"
+curl -s -H "Content-Type: application/json" "$1" -X POST -T "$2"
