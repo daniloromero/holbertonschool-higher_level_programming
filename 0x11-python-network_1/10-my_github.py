21b04fb9d4fb395091de30c92a90e2e0cfbc1f17
daniloromero <1601@holbertonschool.com>
2021-01-11 23:13:53 -0500

    Test

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script takes Github (username and token) uses Github API to display id"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = ' https://api.github.com' 
+    if len(sys.argv) > 1:
+        data = {'username': sys.argv[1]
+                'password': sys.argv{2}}
+    else:
+        data = {'q': ""}
+    try:
+        r = requests.post(url, data)
+        answer = r.json()
+        if answer:
+            print('{}'.format(answer.get('user')))
+        else:
+            print('None')
+    except ValueError:
+        print('Not a valid JSON')
