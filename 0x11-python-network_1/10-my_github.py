53dc7c425af2522768c6c43b21061bdc36e72da6
daniloromero <1601@holbertonschool.com>
2021-01-11 23:53:22 -0500

    Test

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """script takes Github (username and token) uses Github API to display id"""
 import requests
+from requests.auth import HTTPBasicAuth
 import sys
 
 
 if __name__ == '__main__':
     url = ' https://api.github.com' 
     if len(sys.argv) > 1:
-        data = {'username': sys.argv[1],
-                'password': sys.argv[2]}
+        username = sys.argv[1]
+        password = sys.argv[2]
     else:
         data = {'q': ""}
-    try:
-        r = requests.post(url, data)
-        answer = r.json()
-        if answer:
-            print('{}'.format(answer.get('user')))
-        else:
-            print('None')
-    except ValueError:
-        print('Not a valid JSON')
+    r = requests.post(url, auth=HTTPBasicAuth(username, password)
+    answer = r.json()
+    if answer:
+        print('{}'.format(answer.get('user')))
+    else:
+        print('None')
