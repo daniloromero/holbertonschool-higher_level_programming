86b91dbb02e0d6b019f28a9fe7683a011205f11b
daniloromero <1601@holbertonschool.com>
2021-01-11 22:44:11 -0500

    Test

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,11 +7,11 @@ import sys
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) > 1:
-        q = sys.argv[1]
+        data = 'q': sys.argv[1]
     else:
-        q = ""
+        data = {'q': ""}
     try:
-        r = requests.post(url, q)
+        r = requests.post(url, data)
         answer = r.json
         if answer:
             print('[{}] {}'.format(answer.id, answer.name))
