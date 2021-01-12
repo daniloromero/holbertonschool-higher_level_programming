fc9c3fc2968800f05ed3b99f3b64be73b6ffb26d
daniloromero <1601@holbertonschool.com>
2021-01-11 21:53:46 -0500

    Test

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script takes in URL, sends request to URL, displays value of X-Request-Id"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    r = requests.get(url)
+    r = request.post(url, data={'email': sys.argv[2]})
+    body = r.text
+    print(body)
