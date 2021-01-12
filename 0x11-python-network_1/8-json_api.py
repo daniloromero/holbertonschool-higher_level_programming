e9f0cf7709d0fca05325ac16c79c11f2aec49d4d
daniloromero <1601@holbertonschool.com>
2021-01-11 22:31:40 -0500

    Test

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script takes in URL, sends request to URL, displays value of X-Request-Id"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    try:
+        r = requests.post(url, data)
+        answer = r.json
+        if answer:
+            print('[{}] {}'.format(answer.id, answer.name))
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
