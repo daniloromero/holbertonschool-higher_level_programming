eac7cc2466d8f31f94e869907e25783264e53cbc
daniloromero <1601@holbertonschool.com>
2021-01-12 20:55:42 -0500

    Test

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script takes Github (username and token) uses Github API to display id"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        repo = sys.argv[1]
+        owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = requests.get(url)
+    answer = r.json()
+    if answer:
+        for i in answer[0:10]:
+            sha = i.get('sha')
+            author = i.get('author')
+            if sha:
+                print('{}: {}'.format(sha, author.get('login'))
+    else:
+        print('None')
